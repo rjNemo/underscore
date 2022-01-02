b82129f6553306826a2c4d548a95491d243d5005
Ruidy <rnemausat@newstore.com>
2022-01-02 19:14:37 -0400

    fix: example

--- examples/chaining.go
+++ examples/chaining.go
@@ -3,11 +3,11 @@ package examples
 import (
 	"fmt"
 
-	u "github.com/rjNemo/underscore"
+	"github.com/rjNemo/underscore/chain"
 )
 
 func chaining() {
-	sum := u.NewChain([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}).
+	sum := chain.Of([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}).
 		// filter even numbers from the slice
 		Filter(func(n int) bool { return n%2 == 0 }).
 		// square every number in the slice
