28f85bdc94edfba9c92a8d892b3080581765335a
Ruidy <nemausatpro@gmail.com>
2022-02-12 16:08:46 -0400

    use comparable constraint in contains (#12)
    Co-authored-by: Ruidy <ruidy.nemausat@gmail.com>

--- contains.go
+++ contains.go
@@ -1,9 +1,7 @@
 package underscore
 
-import "constraints"
-
 // Contains returns true if the value is present in the slice
-func Contains[T constraints.Ordered](values []T, value T) bool {
+func Contains[T comparable](values []T, value T) bool {
 	for _, v := range values {
 		if v == value {
 			return true
