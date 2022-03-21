0119de7b1ecfe4827bc503357f9feb29df1ec01b
Ruidy <rnemausat@newstore.com>
2022-03-21 12:55:14 -0400

    add documentation

--- drop.go
+++ drop.go
@@ -1,5 +1,7 @@
 package underscore
 
+// Drop returns the rest of the elements in a slice.
+// Pass an index to return the values of the slice from that index onward.
 func Drop[T any](values []T, index int) (rest []T) {
 	for i, value := range values {
 		if i != index {
