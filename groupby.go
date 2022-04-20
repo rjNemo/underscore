5acf4a46716b8d7074d25a692059d48f4bccaf29
Ruidy <rnemausat@newstore.com>
2022-04-20 15:10:40 -0400

    add groupBy function

---
+++ groupby.go
@@ -0,0 +1,15 @@
+package underscore
+
+// GroupBy splits a slice into a map[K][]V grouped by the result of the iterator function.
+func GroupBy[K comparable, V any](values []V, f func(V) K) map[K][]V {
+	res := make(map[K][]V, 0)
+	for _, v := range values {
+		k := f(v)
+		if r, ok := res[k]; ok {
+			res[k] = append(r, v)
+		} else {
+			res[k] = []V{v}
+		}
+	}
+	return res
+}
