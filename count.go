291df4fe4e06e5ea4f29a612af7c6531e9e004bb
Andy Long <andy@theouteredge.co.uk>
2022-11-30 12:34:01 +0000

    Adding a Count function (#34)
    * Adding some new funky functions which I find useful
    
    Created a Tuple struct as some of the new functions require you to return a new slice with two fields which is the result of the new functions
    
    Created the Join, JoinProjection, Range, SumMap, Zip functions, ecah fuction is documented with how it works and had a unit test or maybe more
    
    * Added in an OrderBy function
    
    * Documentation comment for OrderBy which I missed out
    
    * Adding a Unit test for JoinProject function
    
    Updated the comments on the Join & OrderBy functions so they make a little more sense.
    
    Covered an extra test case with the Join test, where the left set has more data than the right and so the Right handside array of the join is empty
    
    * Adding a count method to the package, so you can find out how many items in a slice satisfy and given condition

---
+++ count.go
@@ -0,0 +1,13 @@
+package underscore
+
+// Count returns the number of elements in the slice that satisfy the predicate.
+// example: Count([]int{1,2,3,4,5}, func(n int) bool { return n%2 == 0 }) // 2
+func Count[T comparable](slice []T, predicate func(T) bool) int {
+	count := 0
+	for _, item := range slice {
+		if predicate(item) {
+			count++
+		}
+	}
+	return count
+}
